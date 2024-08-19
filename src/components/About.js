35a7d3cb7b308d21eed0cdbf2510a9cff5d1d289
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-08-19 20:01:02 +0300

    Finished Lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About me</h2>
+      <p>This should be anything</p>
+      <img src={image} alt="I made this"/>
+    </div>
+  );
 }
 
 export default About;
